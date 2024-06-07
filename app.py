bdc2bcddaf67483e960124a6e0e4849da8b23fc5
Samuel Lyon <54977494+ThyEpicPotato@users.noreply.github.com>
2024-06-06 23:04:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from sample import lucene_index
 
 app = Flask(__name__)
 
-REDDIT_BASE_URL = "https://www.reddit.com"
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,7 +16,7 @@ def search():
         formatted_results = [{
             'title': res['title'],
             'content': (res['content'][:200] + '...') if len(res['content']) > 200 else res['content'],
-            'permalink': REDDIT_BASE_URL + res['permalink'] if not res['permalink'].startswith('http') else res['permalink']
+            'permalink': res.get('permalink', '#')
         } for res in results[:10]]
         return jsonify(formatted_results)
     return jsonify([])
